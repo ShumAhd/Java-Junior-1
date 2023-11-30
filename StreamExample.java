6bd6f47fba52fc4d5848d4585ea1b20ae354146b
ShuMAhd <shuman@zykaika.ru>
2023-11-30 09:47:40 +0800

    Создание рандомнымх чисел StreamExample

--- StreamExample.java
+++ StreamExample.java
@@ -1,3 +1,19 @@
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class StreamExample {
+
+  /**
+   * Создает список из 1000 рандомных чисел от 1 до 1000000.
+   *
+   * @return Список из 1000 рандомных чисел.
+   */
+  public static List<Integer> generateRandomNumbers() {
+    Random random = new Random();
+    return random.ints(1000, 1, 1000001)
+        .boxed()
+        .collect(Collectors.toList());
+  }
 
 }
